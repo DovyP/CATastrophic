d9e405b16efa2ca89107a40491ff253e70fe3b46
DESKTOP-2C6KBOK\dovpe <translatefunmira@gmail.com>
2024-01-12 00:35:35 +0200

    Add Player Step sound with random pitch

---
+++ Assets/Scripts/PlayerStepsAudio.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class PlayerStepsAudio : MonoBehaviour
+{
+    [SerializeField] private float pitchRandomness = .05f;
+    [SerializeField] private AudioClip stepClip;
+    private float basePitch;
+    
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Start()
+    {
+        basePitch = audioSource.pitch;
+    }
+
+    private void PlayAudioClipWithPitch(AudioClip audioClip)
+    {
+        if (stepClip != null)
+        {
+            float randomPitch = Random.Range(-pitchRandomness, pitchRandomness);
+            audioSource.pitch = basePitch + randomPitch;
+            PlayAudioClip(audioClip);
+        }
+    }
+
+    private void PlayAudioClip(AudioClip audioClip)
+    {
+        audioSource.Stop();
+        audioSource.clip = audioClip;
+        audioSource.Play();
+    }
+
+    public void PlayStepSound()
+    {
+        PlayAudioClipWithPitch(stepClip);
+    }
+}
